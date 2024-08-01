090311b9cf73128a36e7637f94b762b05eaf185a
Fhellps <159947424+Fhellps@users.noreply.github.com>
2024-07-31 23:45:21 -0300

    suba

---
+++ script.js
@@ -0,0 +1,4 @@
+// Aqui você pode adicionar qualquer funcionalidade JavaScript adicional, se necessário.
+document.getElementById('video').addEventListener('play', function() {
+    console.log('O vídeo começou a tocar');
+});
